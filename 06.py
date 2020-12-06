d1b5504d8640dd50df61293618726d8329b389bf
Marcus Frodin <marcus.frodin@gmail.com>
2020-12-06 09:57:47 -0500

    added.

--- 06.py
+++ 06.py
@@ -1,5 +1,2 @@
-from utils import read_input_multi
-from operator import and_, or_
-from functools import reduce
-
-print([sum(l) for l in zip(*[(len(reduce(or_, c)), len(reduce(and_, c))) for c in [[set([c for c in p]) for p in g] for g in read_input_multi('\n\n','\n')]])])
+print(*map(sum, zip(*[(len(set.union(*p)), len(set.intersection(*p))) 
+    for p in [list(map(set, l.split())) for l in open('06.input', 'r').read().split('\n\n')]])))
