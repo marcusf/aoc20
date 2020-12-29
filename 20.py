df7e6d080fa01b37d226181b7659369d47b0dc69
Marcus Frodin <marcus.frodin@gmail.com>
2020-12-28 20:16:31 -0500

    Made 20 faster.

--- 20.py
+++ 20.py
@@ -1,157 +1,161 @@
 import utils
 import math
 from collections import defaultdict
+import functools
 
 def flipv(grid): return list(reversed(grid))
 def fliph(grid): return [''.join(list(reversed(row))) for row in grid]
 def rot90(grid): return [''.join(l) for l in list(map(list, zip(*grid)))]
+def crop(grid): return [row[1:-1] for row in grid[1:-1]]
 
-def valign(box1, box2):
-    return box1[-1] == box2[0] 
-
-def halign(box1, box2):
-    return [b[-1] for b in box1] == [b[0] for b in box2]
-
-def surrounding(x,y,w):
-    return [(x,y) for x,y in [(x+1, y), (x-1,y), (x,y+1),(x,y-1)] if x>=0 and x<w and y>=0 and y<w]
-
-def next(x,y,w):
-    if (x == w-1 and y % 2 == 0) or (x == 0 and y % 2 == 1):
-        y = y+1
-        x = -1 if y % 2 == 0 else w
-    if y % 2 == 0:
-        x += 1
-    else:
-        x -= 1
-    return (x,y)
-
-def fmt(i,n): return '{}_{}'.format(int(i[5:-1]),n)
+def fmt(i,n): return '{}:{}'.format(int(i[5:-1]),n)
+def row(title, operation, grid):
+    # To avoid costly compares in connected_grid,
+    # we hash each of the edges and compare the hashes.
+    t = fmt(title, operation)
+    return (t, grid, int(t[0:4]), hash_grid(grid))
 
 def build_grids(tiles):
     grids = []
-
-    # Build all versions of each grid
     for title, *grid in tiles:
-        grids += [(fmt(title, 'a'),grid), (fmt(title, 'b'),flipv(grid)), (fmt(title, 'c'),fliph(grid)), \
-            (fmt(title, 'd'), flipv(fliph(grid))), (fmt(title, 'e'), rot90(grid)), \
-            (fmt(title, 'f'), fliph(rot90(grid))), (fmt(title, 'g'), flipv(rot90(grid))), \
-            (fmt(title, 'h'), flipv(fliph(rot90(grid))))]
-
+        grids += [row(title, 'normal', grid),\
+                 row(title, 'flipv', flipv(grid)),\
+                 row(title, 'fliph', fliph(grid)),\
+                 row(title, 'flipv:fliph', flipv(fliph(grid))),\
+                 row(title, 'rot90', rot90(grid)),\
+                 row(title, 'fliph:rot90', fliph(rot90(grid))),\
+                 row(title, 'flipv:rot90', flipv(rot90(grid)))]
     return grids
 
-def validate(solution, grids, width):
-    grids = { title: grid for (title, grid) in grids }
-    matrix = utils.chunk(solution, width)
-    for y, row in enumerate(matrix):
-        for x, col in enumerate(row):
-            if x<len(row)-1 and not halign(grids[matrix[y][x]], grids[matrix[y][x+1]]):
-                return False
-            if x>0 and not halign(grids[matrix[y][x-1]], grids[matrix[y][x]]):
-                return False
-            if y<len(matrix)-1 and not valign(grids[matrix[y][x]], grids[matrix[y+1][x]]):
-                return False
-            if y>0 and not valign(grids[matrix[y-1][x]], grids[matrix[y][x]]):
-                return False 
+def hash_grid(g):
+    rg = rot90(g)
+    return [hash(''.join(gr)) for gr in [g[0], g[-1], rg[0], rg[-1]]]
+
+def valign(box1, box2):
+    return box1[-1] == box2[0]
+
+def halign(box1, box2):
+    for i in range(len(box1)):
+        if not box1[i][-1] == box2[i][0]:
+            return False
     return True
 
-def a(grids, width):
-    candidates = defaultdict(set)
+def connected(label1, hash1, label2, hash2):
+    return (label1 != label2) and (hash1[0] == hash2[1] or hash1[1] == hash2[0] \
+        or hash1[2] == hash2[3] or hash1[3] == hash2[2])
 
-    # Build all versions of each grid
-    for title, *grid in tiles:
-        grids += [(fmt(title, 'a'),grid), (fmt(title, 'b'),flipv(grid)), (fmt(title, 'c'),fliph(grid)), \
-            (fmt(title, 'd'), flipv(fliph(grid))), (fmt(title, 'e'), rot90(grid)), \
-            (fmt(title, 'f'), fliph(rot90(grid))), (fmt(title, 'g'), flipv(rot90(grid))), \
-            (fmt(title, 'h'), flipv(fliph(rot90(grid))))]
+def connect(list, a1, a2):
+    list[a1].append(a2)
+    list[a2].append(a1)
 
-    # Find matching pairs
-    for i, (idd, grid) in enumerate(grids):
-        for idd2, grid2 in grids[i:]:
-            if idd[0:4] == idd2[0:4]:
-                continue
-            if valign(grid, grid2) or halign(grid, grid2) or valign(grid2, grid) or halign(grid2, grid):
-                candidates[idd].add(idd2)
-                candidates[idd2].add(idd)
+def connected_grids(grids):
+    candidates = defaultdict(list)
+    [connect(candidates,k1,k2) for i, (k1, _, l1, h1) in enumerate(grids) for k2, _, l2, h2 in grids[i:] if connected(l1, h1, l2, h2)]
+    return candidates
 
-    for k, _ in sorted(candidates.items(), key=lambda l: len(l[1])):
-        matches = []
-        grid = [0 for _ in range(width*width)]
-        x,y = 0, 0
-        grid[y*width+x] = k
-        q = [(grid,k,x,y)]
+def is_complete(x,y,w):
+    return y == w-1 and ((y % 2 == 0 and x == w-1) or (y % 2 == 1 and x == 0))
 
-        visited = set([tuple(grid)])
-        while q:
-            grid, last, x, y = q.pop(0)
-            xx, yy = next(x,y,width)
-            if y == width-1 and ((y % 2 == 0 and x == width-1) or (y % 2 == 1 and x == 0)) and validate(grid, grids, width):
-                matches.append(grid)
+def aligns(g1,x1,y1,g2,x2,y2):
+    if x1 == x2: return valign(g1,g2) if y1 < y2 else valign(g2,g1)
+    if y1 == y2: return halign(g1,g2) if x1 < x2 else halign(g2,g1)
 
-            for c in candidates[last]:
+def next(x,y,w):
+    if (x == w-1 and y % 2 == 0) or (x == 0 and y % 2 == 1):
+        y += 1
+        x = -1 if y % 2 == 0 else w
+    x = x + 1 if y % 2 == 0 else x - 1
+    return (x,y)
 
-                surr = surrounding(xx,yy,width)
-                if all([c in candidates[grid[sy*width+sx]] for (sx,sy) in surr if grid[sy*width+sx] != 0]) and c not in grid:
-                    grid = grid[:]
-                    grid[yy*width+xx] = c
-                    if not tuple(grid) in visited:
-                        visited.add(tuple(grid))
-                        q.append((grid,c,xx,yy))
-                    else:
-                        grid[yy*width+xx] = 0
-        if matches:
-            return matches
+def dfs(start, grids, candidates, width):
+    grid = [0 for _ in range(width**2)]
+
+    grid[0] = start
+    q = [(grid,start,0,0)]
+
+    visited = set()
+
+    while q:
+        grid, last, x, y = q.pop()
+        
+        if is_complete(x, y, width): return grid
+        if tuple(grid) in visited: continue
+        visited.add(tuple(grid))
+
+        xx, yy = next(x,y,width)
+        for c in candidates[last]:
+            if c not in grid and aligns(grids[last],x,y,grids[c],xx,yy):
+                grid = grid[:]
+                grid[yy*width+xx] = c
+                q.append((grid,c,xx,yy))
+
     return None
 
-PATTERN = '''                  #
-#    ##    ##    ###
- #  #  #  #  #  #'''.split('\n')
-
-def rr(grid): return [l for l in list(map(list, zip(*grid)))]
-def flh(grid): return [list(reversed(row)) for row in grid]
-
-def crop(image): return [row[1:-1] for row in image[1:-1]]
-
-def b(grids, ordering, width):
-    grid = { title: grid for (title, grid) in grids }
+def make_pictures(grids, ordering, width):
+    grid = { title: grid for (title, grid, _, _) in grids }
     pp = [row for i, row in enumerate(utils.chunk(ordering, width))]
-
     joined = [[crop(grid[x]) for x in row] for row in pp] 
 
     picture = []
-    for block0 in joined:
-        for i in range(len(block0[0])):
-            picture.append(''.join([block[i] for block in block0]))
+    for blocks in joined:
+        for i in range(len(blocks[0])):
+            picture.append(''.join([block[i] for block in blocks]))
 
+    return [flipv(fliph(rot90(picture))), flipv(picture), fliph(picture), \
+            flipv(fliph(picture)), rot90(picture),\
+            fliph(rot90(picture)), flipv(rot90(picture)),picture]
+
+def make_pattern():
+    PATTERN = '''                  #
+#    ##    ##    ###
+ #  #  #  #  #  #'''.split('\n')
     pattern = { (y,x) for y, row in enumerate(PATTERN) for x, col in enumerate(row) if col == '#' }
     pw = len(PATTERN[0])+1
+    return pattern, pw
 
-    pictures = [picture, flipv(picture), fliph(picture), flipv(fliph(picture)), rot90(picture), \
-            fliph(rot90(picture)), flipv(rot90(picture)), flipv(fliph(rot90(picture)))]
-    
-    
-    for qq, picture in enumerate(pictures):
-        matches = 0
-        start = 0
-        for r0,r1,r2 in utils.window(picture, 3):
-            for i in range(0, len(r0)-pw):
-                candidate = [r0[i:i+pw], r1[i:i+pw], r2[i:i+pw]]
-                found = True
-                for y,x in pattern:
-                    if not candidate[y][x] == '#':
-                        found = False
-                if found:
-                    matches+=1
-                    spacer = ''.join(['+' if n in range(i,i+len(PATTERN[0])+1) else '-' for n in range(len(picture[0]))])
-            start += 1
+def scan_one(r0,r1,r2,pattern,width):
+    matches = 0
+    for i in range(0, len(r0)-width):
+        candidate = [r0[i:i+width], r1[i:i+width], r2[i:i+width]]
+        found = True
+        for y,x in pattern:
+            if not candidate[y][x] == '#':
+                found = False
+        if found:
+            matches+=1
+    return matches
+
+def a(grids, width):
+    graph = connected_grids(grids)
+    tiles = { title: grid for (title, grid, _, _) in grids }
+    corners = [k for k, v in graph.items() if len(v) == 2]
+
+    for corner in corners:
+        r = dfs(corner, tiles, graph, width)
+        if r: 
+            ints = [int(rr[0:4]) for rr in r]
+            return r, ints[0]*ints[width-1]*ints[width**2-width]*ints[width**2-1]
+
+    return None
+
+def b(grids, ordering, pic_width):
+    pictures = make_pictures(grids, ordering, pic_width)
+    pattern, width = make_pattern()
+
+    for picture in pictures:
+        matches = sum([scan_one(r0,r1,r2,pattern,width) for r0,r1,r2 in utils.window(picture, 3)])
         if matches:
-            print(sum([1 for row in picture for c in row if c == '#']) - matches*len(pattern))
+            return sum([1 for row in picture for c in row if c == '#']) - matches*len(pattern)
 
-tiles = utils.read_input_multi(delim_1='\n\n', delim_2='\n',test=False)
-width = int(math.sqrt(len(tiles)))
-grids = build_grids(tiles)
-result = a(grids, width)[0]
+def main():
+    tiles = utils.read_input_multi(delim_1='\n\n', delim_2='\n',test=False)
+    width = int(math.sqrt(len(tiles)))
+    grids = build_grids(tiles)
 
-r = [int(rr[0:4]) for rr in result]
-print(r[0]*r[width-1]*r[width*width-width]*r[width*width-1])
+    pattern, a_res = a(grids, width)
+    b_res = b(grids, pattern, width)
 
-b(grids, result, width)
+    print(a_res, a_res == 17148689442341)
+    print(b_res, b_res == 2009)
+
+main()
